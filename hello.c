f03c680c8450ab5ea8f1569b5c8fef0a6c8e786b
arrow9484 <akhandelwal2@huskers.unl.edu>
2021-08-26 22:08:18 +0000

    initial commit

---
+++ hello.c
@@ -0,0 +1,18 @@
+/**
+ * Author: Arnav Khandelwal and Mohammed Sbai
+ *         akhandelwal2@huskers.unl.edu
+ *         msbai2@huskers.unl.edu
+ * Date: 2021/08/24
+ *
+ * A simple program to print your name in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Hello our names are Mohammed and Arnav!\n");
+
+  return 0;
+}
